63b3bb68b54b7fdfba1fe88fed775ca9e4d9578c
Gustavo Assis <guu.assis@hotmail.com>
2023-04-09 00:05:05 -0300

    Tela gameover concluida
    imagens alterando de acordo com um valor booleano

--- Toim.swiftpm/Views/GameOverView.swift
+++ Toim.swiftpm/Views/GameOverView.swift
@@ -8,41 +8,88 @@
 import SwiftUI
 
 struct GameOverView: View {
+    @State var highScore: Bool = false
+    
     var body: some View {
         GeometryReader { geometry in
-
-           //Buttons
             ZStack{
-                VStack {
-                    Spacer()
+                Color(.black)
+                    .opacity(0.6)
+            }
+            //Buttons
+            VStack {
+                ZStack {
                     RoundedRectangle(cornerRadius: 30)
-                        .padding(.top, 100)
+                        .foregroundColor(.white)
+                        .opacity(0.4)
+                    
+                    VStack(spacing: 0) {
+                        HStack {
+                            Text("Você fez 19 pontos")
+                        }.frame(height: geometry.size.height * 0.1)
+                        Divider()
+                        HStack {
+                            Button {
+                                print("tryagainn")
+                            } label: {
+                                Text("Try again")
+                                    .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.1)
+                            }
+                            
+                        }.frame(height: geometry.size.height * 0.1)
+                        Divider()
+                        HStack {
+                            Button {
+                                print("Quit")
+                            } label: {
+                                Text("Quit")
+                                    .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.1)
+                            }
+                            
+                        }.frame(height: geometry.size.height * 0.1)
+                    }.font(.system(.title))
+                        .foregroundColor(.black)
+                    
                 }
-                .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.45)
+                .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.3)
+                .padding(.top, geometry.size.height * 0.1)
+                
             }
             .frame(width: geometry.size.width)
             
             //Image
-
-                VStack{
+            VStack{
+                if highScore {
                     Image("record")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: geometry.size.width, height: geometry.size.height * 0.15)
+                } else {
+                    Image("gameOver")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geometry.size.width, height: geometry.size.height * 0.15)
                 }
-                .padding(.top, 30)
-
+                
+            }
+            .padding(.top, 30)
             
+            
+            //Image
             VStack {
-                //Image
-                ZStack{
-                        VStack{
-                            Image("toim-record")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                        }
+                VStack{
+                    if highScore {
+                        Image("toim-record")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } else {
+                        Image("toim-gameOver")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
                 }
-                .frame(height: geometry.size.height * 0.6 )
+                .frame(height: geometry.size.height * 0.6)
+                
             }
             .frame(width: geometry.size.width, height: geometry.size.height * 0.98, alignment: .bottom)
         } .ignoresSafeArea()
